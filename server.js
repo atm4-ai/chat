cfbb1b9b7c76fa2ee9772a9c71deeb40353cbff9
atm4-ai <m13122922773@gmail.com>
2025-04-06 02:42:00 +0900

    new1
    new1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// serve front-end
+// Serve static frontend from /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
