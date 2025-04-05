4fdcc9c0528c75ebfa7f02bc4415a597d81c7bdc
atm4-ai <m13122922773@gmail.com>
2025-04-06 05:46:46 +0900

    23
    23

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const path = require('path');
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server);
 
-const users = new Map();
+let users = [];
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => {
-  console.log('New user connected');
+io.on("connection", (socket) => {
+  let currentUser = null;
 
-  socket.on('join', (user) => {
-    users.set(socket.id, user);
-    io.emit('userList', Array.from(users.values()));
+  socket.on("join", (user) => {
+    currentUser = user;
+    users.push({ ...user, id: socket.id });
+    io.emit("userList", users);
   });
 
-  socket.on('chatMessage', (msg) => {
-    const user = users.get(socket.id);
-    if (user) {
-      io.emit('chatMessage', {
-        name: user.name,
-        avatar: user.avatar,
-        text: msg,
-        time: new Date().toLocaleTimeString()
-      });
+  socket.on("chatMessage", (msg) => {
+    if (currentUser) {
+      io.emit("chatMessage", { ...currentUser, text: msg });
     }
   });
 
-  socket.on('disconnect', () => {
-    users.delete(socket.id);
-    io.emit('userList', Array.from(users.values()));
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("userList", users);
   });
 });
 
