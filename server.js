607ffe792fd2fc82b41b5bdebc5040a0eb9f4ae3
atm4-ai <m13122922773@gmail.com>
2025-04-06 03:54:13 +0900

    123
    123

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Full socket.io + user profile + room support server.js will go here
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+let onlineUsers = {};
+
+io.on('connection', (socket) => {
+  socket.on('user join', (userData) => {
+    onlineUsers[socket.id] = userData;
+    io.emit('user list', Object.values(onlineUsers));
+  });
+
+  socket.on('chat message', (msgData) => {
+    io.emit('chat message', msgData);
+  });
+
+  socket.on('disconnect', () => {
+    delete onlineUsers[socket.id];
+    io.emit('user list', Object.values(onlineUsers));
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
